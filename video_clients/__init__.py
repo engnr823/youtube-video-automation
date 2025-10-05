b3f6bd491bbe38a9efc11007fb63f2bd15207194
engnr823 <engnr823@gmail.com>
2025-10-05 11:49:31 +0400

    Create __init__.py

---
+++ video_clients/__init__.py
@@ -0,0 +1,4 @@
+# This file can be left empty.
+# Its presence makes the 'video_clients' directory a Python package,
+# which allows you to import files from it, like:
+# from video_clients.elevenlabs_client import generate_voiceover_and_upload
