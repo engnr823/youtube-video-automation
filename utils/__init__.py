5c8455608353a048d9f162fdce167fede2438530
engnr823 <engnr823@gmail.com>
2025-10-05 11:51:33 +0400

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+# This file can be left empty.
+# Its presence makes the 'utils' directory a Python package,
+# which allows you to import files from it, like:
+# from utils.ffmpeg_utils import stitch_video_with_audio
